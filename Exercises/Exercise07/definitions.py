f8f82473e6696f0b87d003b12bc10912d8ec4641
Eric Risbakk <e.risbakk@student.maastrichtuniversity.nl>
2017-10-04 16:59:28 +0200

    Starded doing exercise 7, completed it. For some reason it seems to be slower than the regular one. Maybe the software I have is exceptionally slow in that regard?
    Further testing should be done on the server.

---
+++ Exercises/Exercise07/definitions.py
@@ -0,0 +1,18 @@
+
+# Order of the square matrices A, B and C
+ORDER = 200
+
+# A elements are constant and equal to AVAL
+AVAL = 3.0
+
+# B elements are constant and equal to BVAL
+BVAL = 5.0
+
+# tolerance used in floating point comparisons
+TOL = 0.001
+
+# Max dim for NDRange
+DIM = 2
+
+# number of times to do each multiplication
+COUNT = 1
